c313116a7c146e36ba2d94b09d2a1774eead2ee9
Tim Kellogg <timothy.kellogg@gmail.com>
2021-12-31 16:08:00 -0500

    Added sub-commands

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
 mod snapshots;
 
 use std::path::Path;
+use std::process;
 
 fn main() {
-    if let Some(oid) = snapshots::create(Path::new(".")).unwrap() {
-        println!("{}", oid);
+    let args: Vec<String> = std::env::args().collect();
+    if args.get(1) == Some(&"capture".to_string()) {
+        if let Some(oid) = snapshots::create(Path::new(".")).unwrap() {
+            println!("{}", oid);
+        }
+    } else {
+        dbg!(args);
+        eprintln!("Usage: duralumin capture");
+        process::exit(1);
     }
 }
