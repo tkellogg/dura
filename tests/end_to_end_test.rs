bca30983ff82c42575942357414e74fa055d4448
Dee Anzorge <d.anzorge@gmail.com>
2022-01-08 23:58:21 +0100

    Make test use tmpdir for saving config (#54)

--- tests/end_to_end_test.rs
+++ tests/end_to_end_test.rs
@@ -20,13 +20,13 @@ fn start_serve() {
 
 #[test]
 fn start_serve_with_null_pid_in_config() {
+    let mut dura = util::dura::Dura::new();
     let mut cfg = Config::empty();
     cfg.pid = None;
-    cfg.save();
+    dura.save_config(&cfg);
 
-    let mut dura = util::dura::Dura::new();
     assert_eq!(None, dura.pid(true));
-    assert_eq!(None, dura.get_config());
+    assert_ne!(None, dura.get_config());
 
     dura.start_async(&["serve"], true);
     dura.wait();
