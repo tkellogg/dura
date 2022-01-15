df59281b97870f509e558f00adb5e4e4f14f4789
Pol Dellaiera <pol.dellaiera@protonmail.com>
2022-01-15 20:58:39 +0100

    tests: Disable local git option about gpg signing if any.

--- tests/util/git_repo.rs
+++ tests/util/git_repo.rs
@@ -60,13 +60,18 @@ impl GitRepo {
         let _ = self.git(&["checkout", "-b", "master"]).unwrap();
         // Linux & Windows will fail on `git commit` if these aren't set
         let _ = self.git(&["config", "user.name", "duratest"]).unwrap();
-        let _ = self.git(&["config", "user.email", "duratest@dura.io"]).unwrap();
+        let _ = self
+            .git(&["config", "user.email", "duratest@dura.io"])
+            .unwrap();
     }
 
     pub fn commit_all(&self) {
         self.git(&["add", "."]).unwrap();
         self.git(&["status"]).unwrap();
-        self.git(&["commit", "-m", "test"]).unwrap();
+        // We disable gpg signing to avoid interfering with local global
+        // ~/.gitconfig file, if any.
+        self.git(&["commit", "--no-gpg-sign", "-m", "test"])
+            .unwrap();
     }
 
     pub fn write_file(&self, path: &str) {
