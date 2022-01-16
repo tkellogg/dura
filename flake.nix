8e4a34044f803315988080c1dfb1ca8a84d0dd55
Pol Dellaiera <pol.dellaiera@protonmail.com>
2022-01-16 21:47:13 +0100

    chore: Update `flake.nix`.

--- flake.nix
+++ flake.nix
@@ -1,31 +1,63 @@
 {
-  description = "Dura development environment";
+  description = "Dura build and development environment";
 
+  # Provides abstraction to boiler-code when specifying multi-platform outputs.
   inputs = {
-    nixpkgs.url      = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.url  = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs {
-          inherit system overlays;
+        mkPkgs = pkgs: extraOverlays: import pkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
         };
+
+        pkgs = mkPkgs nixpkgs [ rust-overlay.overlay ];
       in
-      with pkgs;
-      {
-        devShell = mkShell {
+      rec {
+        packages = flake-utils.lib.flattenTree {
+          dura = pkgs.rustPlatform.buildRustPackage {
+            pname = "dura";
+            version = "unstable-${self.lastModifiedDate}";
+            description = "A background process that saves uncommited changes on git";
+
+            src = self;
+
+            outputs = [ "out" ];
+
+            cargoLock = {
+              lockFile = self + "/Cargo.lock";
+            };
+
+            buildInputs = [
+              pkgs.openssl
+            ];
+
+            nativeBuildInputs = [
+              pkgs.pkg-config
+            ];
+          };
+        };
+
+        defaultPackage = packages.dura;
+        apps = {
+          dura = flake-utils.lib.mkApp { drv = packages.dura; };
+        };
+        defaultApp = apps.dura;
+
+        devShell = pkgs.mkShell {
           RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
 
           buildInputs = [
-            openssl
-            pkgconfig
-            (rust-bin.stable.latest.default.override { extensions = [ "rust-src" ]; })
+            pkgs.openssl
+            pkgs.pkgconfig
+            (pkgs.rust-bin.stable.latest.default.override { extensions = [ "rust-src" ]; })
           ];
         };
-      }
-    );
+
+      });
 }
